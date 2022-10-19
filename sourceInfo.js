4d97f9f512e49e015fdf358fad8fe0ba33fd5000
Lenard Frommelt <lenny09918050@gmail.com>
2022-10-19 13:01:22 +0300

    1

---
+++ sourceInfo.js
@@ -0,0 +1,10 @@
+module.exports = {
+    getDependencies: () => {
+        return {
+            "vanillaqr": "^1.0.0",
+            "qr-scanner": "^1.4.1",
+            "thingy-network-base": "^0.0.4",
+            "thingy-byte-utils": "^0.0.7"
+        }
+    }
+}

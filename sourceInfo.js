76319cd320b21996a0c8bbc9dc1cfc169de19c2a
Lenard Frommelt <lenny09918050@gmail.com>
2022-11-10 10:19:44 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,6 +1,7 @@
 module.exports = {
     getDependencies: () => {
         return {
+            "secret-manager-client": "^0.0.6",
             "vanillaqr": "^1.0.0",
             "qr-scanner": "^1.4.1",
             "thingy-network-base": "^0.0.4",
